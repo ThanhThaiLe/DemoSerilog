aa22c9d0bbd75c32bc3c7e5d10facf5890f94210
ThanhThaiLe <ThanhThaiLe>
2024-10-05 14:08:44 +0700

    Demo Serilog

---
+++ DemoSerilog/DemoSerilog/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoSerilog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult Get()
+        {
+            var list_item = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+            for (int i = 0; i < list_item.Count(); i++)
+            {
+                var item = list_item[i];
+                _logger.LogInformation("Log received: {Date} - {TemperatureC} - {Summary}", item.Date, item.TemperatureC, item.Summary);
+            }
+
+            return Ok(list_item);
+        }
+    }
+}
